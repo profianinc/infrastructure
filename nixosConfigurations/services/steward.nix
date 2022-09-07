5a265dadc296c488f9577b7e791b186fe3d15221
Patrick Uiterwijk <patrick@profian.com>
2022-09-07 16:03:29 +0200

    feat: remove attest.profian.com
    This instance has been taken over by the production-{1,2,3,4} instances.
    
    Signed-off-by: Patrick Uiterwijk <patrick@profian.com>

--- nixosConfigurations/services/steward.nix
+++ nixosConfigurations/services/steward.nix
@@ -99,14 +99,6 @@ with flake-utils.lib.system; let
     })
   ];
 
-  attest = mkEC2 [
-    ({...}: {
-      profian.environment = "production";
-
-      networking.domain = "profian.com";
-    })
-  ];
-
   mkProd = n:
     nixpkgs.lib.nixosSystem {
       system = x86_64-linux;
@@ -125,7 +117,6 @@ with flake-utils.lib.system; let
   attest-production-4 = mkProd 4;
 in {
   inherit
-    attest
     attest-staging
     attest-testing
     attest-production-1
