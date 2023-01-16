6bd793a339332bca8d624cdc89e57af4f74a3d75
Patrick Uiterwijk <patrick@profian.com>
2023-01-16 22:43:42 +0100

    chore: update to Enarx 0.7.0
    Signed-off-by: Patrick Uiterwijk <patrick@profian.com>

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs.benefice-testing.url = github:profianinc/benefice;
   inputs.deploy-rs.inputs.flake-compat.follows = "flake-compat";
   inputs.deploy-rs.url = github:serokell/deploy-rs;
-  inputs.enarx.url = github:enarx/enarx; # temporarily use `main` until version above v0.6.3 is released
+  inputs.enarx.url = github:enarx/enarx/v0.7.0;
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
